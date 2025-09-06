50dee507b37fb832faea7cb014983fc865d1305b
Jahan Addison <info@jacata.me>
2025-09-06 13:30:16 -0400

    feat: initial commit

---
+++ examples/prim_example.cpp
@@ -0,0 +1,24 @@
+
+#include <ios>
+#include <iostream>
+#include <simplejson.h>
+
+using json::JSON;
+using namespace std;
+
+int main()
+{
+    JSON obj;
+
+    obj = true;
+    cout << "Value: " << boolalpha << obj.ToBool() << endl;
+
+    obj = "Test String";
+    cout << "Value: " << obj.ToString() << endl;
+
+    obj = 2.2;
+    cout << "Value: " << obj.ToFloat() << endl;
+
+    obj = 3;
+    cout << "Value: " << obj.ToInt() << endl;
+}
